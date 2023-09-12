9aea7a7186750eb82a15653ec3e10d228d6f8415
sakioo <53650208+thomaue@users.noreply.github.com>
2023-09-12 20:18:45 +0200

    Create ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 20:05:31 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/12 20:05:31 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "file.h"
+
+char *strchr(const char *s, int c)
+{
+    while(*s)
+    {
+        if(*s == c)
+            return(char *)s;
+        s++;
+    }
+
+    if(c == '\0')
+        return ((char *)s);
+
+    return (NULL);
+}
+
+// int main(void)
+// {
+//     char *str = "helo les copains";
+
+//     int c = 'c';
+
+//     printf("%s\n", strchr(str, c));
+
+//     return (0);
+// }
