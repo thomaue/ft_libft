dd23caed82f130a2e4bff4b948fcc5d4896f7648
sakioo <53650208+thomaue@users.noreply.github.com>
2023-09-12 19:05:38 +0200

    Create ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 15:06:24 by marvin            #+#    #+#             */
+/*   Updated: 2023/08/28 15:06:24 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "file.h"
+
+void *ft_memset(void *str, int c, size_t n)
+{
+    unsigned char *ptr = (unsigned char *)str;
+
+    while(n > 0)
+    {
+        *ptr++ = (unsigned char)c;
+        n--;
+    }
+    return str;
+}
+
+// int main(void)
+// {
+//     int array [] = {15 , 20, 50, 60 ,42, 42 , 20};
+//     int size = sizeof(int) * 7;
+
+//     for(int size = 0; size < 7; size++)
+//         printf("%d ", array[size]);
+//     printf("\n");
+
+//     ft_memset(array, '0' , size);
+
+//     for(size = 0; size < 7; size++)
+//         printf("%c ", array[size]);
+//     printf("\n");
+
+//     return 0;
+// }
