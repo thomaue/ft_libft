0beb74a9a0ec10650c775357edc09b9da56791cc
sakioo <53650208+thomaue@users.noreply.github.com>
2023-09-12 19:44:07 +0200

    Create ft_isprint.c

---
+++ ft_isprint.c
@@ -0,0 +1,26 @@
+#include "file.h"
+
+int ft_isprint(int c)
+{
+    return(c >= 32 && c <= 127);
+}
+
+//yo
+
+int main(void)
+{
+    // return 0
+
+    printf("0 : %d\n", ft_isprint('\n'));
+    printf("0 : %d\n", ft_isprint('\t'));
+    printf("0 : %d\n\n", ft_isprint('\v'));
+
+    // return 1
+
+    printf("1 : %d\n", ft_isprint('a'));
+    printf("1 : %d\n", ft_isprint('0'));
+    printf("1 : %d\n", ft_isprint('2'));
+    printf("1 : %d\n", ft_isprint(' '));
+
+    return (0);
+}
