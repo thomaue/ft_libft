4c9b7804c7cd75ebc302ca57842f85a339f5a939
sakioo <53650208+thomaue@users.noreply.github.com>
2023-09-12 19:05:39 +0200

    Create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 18:44:11 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/12 18:44:11 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "file.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char *ptr_dst = (unsigned char *)dst;        
+    unsigned char *ptr_src = (unsigned char *)src;
+
+    while(n > 0)
+    {
+        *ptr_dst++ = *ptr_src++;
+        n--;
+    }
+    return (dst);
+}
+
+// int main(void)
+// {
+//     int array [] = {15 , 20, 50, 60 ,42, 42 , 20};
+//     int *num_dst = NULL;
+//     int size = sizeof(int) * 7;
+
+//     num_dst = (int *)malloc(size);
+//     ft_memcpy(num_dst, array, size);
+
+//     for(size = 0; size < 7; size++)
+//         printf("%d - %d\n", array[size], num_dst[size]);
+
+//     free(num_dst);
+//     return 0;
+// }
