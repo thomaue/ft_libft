211cd0afd8953e782b37c5180b360f7574720201
sakioo <53650208+thomaue@users.noreply.github.com>
2023-09-13 13:18:39 +0200

    Create ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 12:52:39 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/13 12:52:39 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "file.h"
+
+void *memchr(const void *s, int c, size_t n)
+{
+    unsigned char *ptr_s = (unsigned char *)s;
+
+    while(n > 0)
+    {
+        if(*ptr_s++ == c)
+            return (ptr_s);
+        n--;
+    }
+    return(NULL);
+}
+
+// int main(void)
+// {
+//     char *str = "botte de foin";
+
+//     int c = 'z';
+
+//     size_t i = 13;
+
+//     printf("%p\n", memchr(str, c, i));
+
+//     return (0);
+// }
