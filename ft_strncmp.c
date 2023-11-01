71c3b0e837233a844f5ae3d63e3d9b618f066794
sakioo <53650208+thomaue@users.noreply.github.com>
2023-11-01 23:09:57 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 23:55:55 by tauer             #+#    #+#             */
-/*   Updated: 2023/11/01 21:47:40 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/01 22:23:39 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	ft_strncmp(const char *s1,const char *s2, size_t n)
 	{
 		if(*s1 != *s2 || *s1 == '\0' || *s2 == '\0')
 			return (*s1 - *s2);
+
 		s1++;
 		s2++;
 		n--;
@@ -27,9 +28,9 @@ int	ft_strncmp(const char *s1,const char *s2, size_t n)
 
 // int	main(void)
 // {
-// 	char *s1 = "found R2A6";
-// 	char *s2 = "found R2A6";
-// 	int	size = -1;
+// 	char *s1 = "test\200";
+// 	char *s2 = "test\0";
+// 	int	size = 6;
 
 // 	printf("mine :%d\n", ft_strncmp(s1, s2, size));
 // 	printf("origin :%d\n", strncmp(s1,s2,size));
