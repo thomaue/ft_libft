48b52b1f1457ea65bf5f3123c4c99873d22f6ca7
sakioo <53650208+thomaue@users.noreply.github.com>
2023-11-09 12:07:59 +0100

    Create ft_lstlast.c

---
+++ ft_lstlast.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tauer <tauer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 03:48:42 by tauer             #+#    #+#             */
+/*   Updated: 2023/11/08 04:12:26 by tauer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+// int	main(void)
+// {
+// 	t_list *first = ft_lstnew("first one");
+// 	t_list *second = ft_lstnew("second one");
+// 	t_list *third = ft_lstnew("third one");
+
+// 	first->next = second;
+// 	second->next = third;
+// 	third->next = NULL;
+
+// 	t_list *stock = ft_lstlast(first);
+
+// 	printf("%s\n", stock->content);
+
+// 	return (0);
+// }
