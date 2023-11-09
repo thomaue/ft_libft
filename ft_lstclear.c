84c87db88cf4ada306abc5baa97bef3e8e79461b
sakioo <53650208+thomaue@users.noreply.github.com>
2023-11-09 12:07:53 +0100

    Create ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tauer <tauer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 07:11:51 by tauer             #+#    #+#             */
+/*   Updated: 2023/11/08 10:33:32 by tauer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = (*lst)->next; 
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+}
+
+void	custom_del_clear(void *content)
+{
+	free(content);
+}
+
+// int	main(void)
+// {
+// 	t_list *first = ft_lstnew(ft_strdup("first"));
+// 	t_list *second = ft_lstnew(ft_strdup("second"));
+// 	t_list *third = ft_lstnew(ft_strdup("third"));
+
+// 	first->next = second;
+// 	second->next = third;
+// 	third->next = NULL;
+
+// 	ft_lstclear(&first, custom_del_clear);
+
+// 	//valgrind --leak-check=full ./a.out
+
+// 	return (0);
+// }
